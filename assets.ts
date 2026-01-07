20f648d80e19d2e5b1e9223c3dd40c9ebe34cc51
Nanton-Kijpadung <nanton.lian@gmail.com>
2026-01-08 02:47:32 +0700

    Add files via upload

---
+++ assets.ts
@@ -0,0 +1,36 @@
+/**
+ * ASSET CONFIGURATION
+ */
+
+export const ASSETS = {
+  logos: {
+    vorta: './vorta-logo.png',
+    geoose: './geoose-logo.png',
+  },
+  robot: {
+    main: './robot-image.png' 
+  },
+  home: {
+    aboutSection: './home-about.png',
+    monitoring: './home-monitoring.jpg',
+    intelligent: './home-intelligent.png',
+    ecoConscious: './home-eco.jpg'
+  },
+  about: {
+    teamCollab: './all-members.jpg',
+    member1: './member1.jpg',
+    member2: './member2.jpg',
+    member3: './member3.jpg',
+    member4: './member4.jpg',
+    member5: './member5.jpg'
+  },
+  howItWorks: {
+    ecosystem: './overview-ecosystem.png',
+    surveyor: './surveyor-robot.png',
+    optimizer: './optimizer-robot.png',
+    dockingStation: './docking-station.png'
+  },
+  impact: {
+    pollutedWater: './impact-water.png'
+  }
+};
