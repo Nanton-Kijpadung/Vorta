8069e0fdb95ad5f36bf91c4a5939a005e632cf33
Nanton-Kijpadung <nanton.lian@gmail.com>
2026-01-08 09:28:04 +0700

    Add files via upload

---
+++ assets.ts
@@ -0,0 +1,37 @@
+/**
+ * ASSET CONFIGURATION
+ * Using the provided GitHub raw assets for Vorta and GEOOSE.
+ */
+
+export const ASSETS = {
+  logos: {
+    vorta: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/vorta-logo.png',
+    geoose: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/geoose-logo.png',
+  },
+  robot: {
+    main: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/robot-image.png' 
+  },
+  home: {
+    aboutSection: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/home-about.png',
+    monitoring: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/home-monitoring.jpg',
+    intelligent: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/home-intelligent.png',
+    ecoConscious: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/home-eco.jpg'
+  },
+  about: {
+    teamCollab: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/all-members.JPG',
+    member1: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/member1.jpg',
+    member2: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/member2.jpg',
+    member3: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/member3.jpg',
+    member4: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/member4.jpg',
+    member5: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/member5.jpg'
+  },
+  howItWorks: {
+    ecosystem: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/overview-ecosystem.png',
+    surveyor: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/surveyor-robot.png',
+    optimizer: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/optimizer-robot.png',
+    dockingStation: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/docking-station.png'
+  },
+  impact: {
+    pollutedWater: 'https://raw.githubusercontent.com/Nanton-Kijpadung/Vorta/refs/heads/main/impact-water.png'
+  }
+};
